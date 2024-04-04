8d21e63a96f0c3a4a427898bfa7a6ba01938ee7e
AKaKLya <67385510+AKaKLya@users.noreply.github.com>
2024-04-04 17:03:14 +0800

    V5.1.1

--- Source/MatHelper/Private/EngineClass/CusAssetDefinition_MatInterface.cpp
+++ Source/MatHelper/Private/EngineClass/CusAssetDefinition_MatInterface.cpp
@@ -5,6 +5,7 @@
 
 #include "ContentBrowserMenuContexts.h"
 #include "IAssetTools.h"
+#include "IContentBrowserSingleton.h"
 #include "MaterialPropertyHelpers.h"
 #include "MaterialEditor/MaterialEditorInstanceConstant.h"
 #include "Materials/MaterialInstanceConstant.h"
@@ -37,56 +38,60 @@ namespace MenuExtension_MatInterface
 	static void ExecuteNewMIC(const FToolMenuContext& MenuContext)
 	{
 		const UContentBrowserAssetContextMenuContext* CBContext = UContentBrowserAssetContextMenuContext::FindContextWithAssets(MenuContext);
-		IAssetTools::Get().CreateAssetsFrom<UMaterialInterface>(CBContext->LoadSelectedObjects<UMaterialInterface>(), UMaterialInstanceConstant::StaticClass(), TEXT("_Inst"), [](UMaterialInterface* SourceObject)
+		auto Objects = CBContext->LoadSelectedObjects<UMaterialInterface>();
+		TArray<UObject*> AssetList;
+		for(auto SourceObject : Objects)
+		{
+			FString TargetPath = SourceObject->GetPathName();
+			const FString BaseName = SourceObject->GetName();
+			TargetPath.ReplaceInline(*BaseName,*FString(""));
+			TargetPath.ReplaceInline(*FString("."),*FString(""));
+
+			FString NewBaseName = BaseName;
+			if(BaseName.Left(2) == "M_")
 			{
-				FString TargetPath = SourceObject->GetPathName();
-				const FString BaseName = SourceObject->GetName();
-				TargetPath.ReplaceInline(*BaseName,*FString(""));
-				TargetPath.ReplaceInline(*FString("."),*FString(""));
-
-				FString NewBaseName = BaseName;
-				if(BaseName.Left(2) == "M_")
-				{
-					NewBaseName.ReplaceInline(*FString("M_"),*FString("MI_"));
-				}
-				else
-				{
-					NewBaseName = "MI_" + BaseName;
-				}
-				int RenameTime = 1;
-				
-				Rename:
-				RenameTime = RenameTime + 1 ;
-				
-				const FString NewName = NewBaseName + "_" + FString::FromInt(RenameTime);
-
-				
-				const FString NewPath = TargetPath + NewName;
-				
-				UEditorAssetSubsystem* EditorAssetSubsystem = GEditor->GetEditorSubsystem<UEditorAssetSubsystem>();
-				if(EditorAssetSubsystem->DoesAssetExist(NewPath))
-				{
-					goto Rename;
-				}
-				
-				UMaterialInstance* NewMi = Cast<UMaterialInstance>(EditorAssetSubsystem->DuplicateAsset("/MatHelper/Material/MI_Empty", NewPath));
-				NewMi->Parent=SourceObject;
-				
-				UMaterialInstanceConstant* ConstMat = static_cast<UMaterialInstanceConstant*>(NewMi);
-				const auto MaterialEditorInstance = NewObject<UMaterialEditorInstanceConstant>(GetTransientPackage(), NAME_None, RF_Transactional);
-				MaterialEditorInstance->SetSourceInstance(ConstMat);
-				for (int32 GroupIdx = 0; GroupIdx < MaterialEditorInstance->ParameterGroups.Num(); ++GroupIdx)
-				{
-					FEditorParameterGroup& ParameterGroup = MaterialEditorInstance->ParameterGroups[GroupIdx];
-					for (int32 ParamIdx = 0; ParamIdx < ParameterGroup.Parameters.Num(); ++ParamIdx)
-					{
-						UDEditorParameterValue* Parameter = ParameterGroup.Parameters[ParamIdx];
-						FMaterialPropertyHelpers::OnOverrideParameter(true,Parameter,MaterialEditorInstance);
-					}
-				}
-				return nullptr;
+				NewBaseName.ReplaceInline(*FString("M_"),*FString("MI_"));
 			}
-		);
+			else
+			{
+				NewBaseName = "MI_" + BaseName;
+			}
+
+			UEditorAssetSubsystem* EditorAssetSubsystem = GEditor->GetEditorSubsystem<UEditorAssetSubsystem>();
+			FString NewName,NewPath;
+		
+			int RenameTime = 0;
+		
+			Rename:
+			RenameTime = RenameTime + 1 ;
+		
+			NewName = NewBaseName + "_" + FString::FromInt(RenameTime);
+			NewPath = TargetPath + NewName;
+		
+		
+			if(EditorAssetSubsystem->DoesAssetExist(NewPath))
+			{
+				goto Rename;
+			}
+		
+			UMaterialInstance* NewMi = Cast<UMaterialInstance>(EditorAssetSubsystem->DuplicateAsset("/MatHelper/Material/MI_Empty", NewPath));
+			NewMi->Parent=SourceObject;
+		
+			UMaterialInstanceConstant* ConstMat = static_cast<UMaterialInstanceConstant*>(NewMi);
+			const auto MaterialEditorInstance = NewObject<UMaterialEditorInstanceConstant>(GetTransientPackage(), NAME_None, RF_Transactional);
+			MaterialEditorInstance->SetSourceInstance(ConstMat);
+			for (int32 GroupIdx = 0; GroupIdx < MaterialEditorInstance->ParameterGroups.Num(); ++GroupIdx)
+			{
+				FEditorParameterGroup& ParameterGroup = MaterialEditorInstance->ParameterGroups[GroupIdx];
+				for (int32 ParamIdx = 0; ParamIdx < ParameterGroup.Parameters.Num(); ++ParamIdx)
+				{
+					UDEditorParameterValue* Parameter = ParameterGroup.Parameters[ParamIdx];
+					FMaterialPropertyHelpers::OnOverrideParameter(true,Parameter,MaterialEditorInstance);
+				}
+			}
+			AssetList.Add(NewMi);
+		}
+		IContentBrowserSingleton::Get().SyncBrowserToAssets(AssetList);
 	}
 	
 	static FDelayedAutoRegisterHelper DelayedAutoRegister(EDelayedRegisterRunPhase::EndOfEngineInit, []{ 
